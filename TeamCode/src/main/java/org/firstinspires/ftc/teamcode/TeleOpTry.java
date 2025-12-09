e1b443a8538edb2dcf5ba967e1053aa1c2966e83
39 <dai.char39@gmail.com>
2025-12-09 15:25:50 +0800

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTry.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class TeleOpTry extends LinearOpMode {
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, " frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+
+        waitForStart();
+        while (opModeIsActive()) {
+
+        }
+    }
+}
